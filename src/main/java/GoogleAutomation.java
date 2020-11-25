c71a3262420b8c90f1d066cc5efaabdaaf88a563
Mohammed Minhaz <mminhaz@ibm.com>
2020-11-24 20:43:09 -0500

    push code

--- src/main/java/GoogleAutomation.java
+++ src/main/java/GoogleAutomation.java
@@ -1,57 +1,80 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
 
 public class GoogleAutomation {
 
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/chromedriverm");
 
         WebDriver driver = new ChromeDriver();
 
-        // Navigate to google
-        driver.get("http://www.google.com"); // will navigate and wait till the entire page is loaded
-        //driver.navigate().to("http://www.google.com"); // Take you to the page and not wait
+        //This maximizes the browser
+        driver.manage().window().maximize();
 
-        //Find text box AND search for selenium
-//		driver.findElement(By.name("q")).sendKeys("Selenium");
-//		driver.findElement(By.name("q")).clear();
+//        //Navigate to the page
+//        driver.get("http://www.google.com"); // 1. This waits for the entire page to load
+//        driver.navigate().to("http://www.google.com"); //2. This doesn't wait for the entire page to load
+//
+//        driver.findElement(By.name("q")).sendKeys("Selenium");
+//    	driver.findElement(By.name("q")).clear();
 //		driver.findElement(By.name("q")).sendKeys("Java");
 
-        //Optimized version
-        WebElement search_box = driver.findElement(By.name("q"));
-        search_box.sendKeys("Selenium");
-//		search_box.clear();
-//		search_box.sendKeys("Java");
 
-        //Find search button
-        WebElement googleSearchButton = driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@name='btnK']"));
+//        //Navigate to home page
+        driver.get("https://momovies.netlify.app/movies");
 
-        //Click search button - click()
-        googleSearchButton.click();
+//        //Click on contact us button
+//        driver.findElement(By.linkText("Contact Us")).click();
+//          driver.findElement(By.xpath("//a[contains(text(), 'Contact Us')]")).click();
+
+          String movieName = "Spider-man";
+//        //Enter John Doe as name
+        driver.findElement(By.id("search-input")).sendKeys(movieName);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//ul[@class=\"search-suggest list-group list-group-flush\"]/li[1]//a")).click();
+
+        //Gets the title from website
+        String actualMovieTitle = driver.findElement(By.xpath("//header/section//h1")).getText();
+
+//        Assert.assertTrue("Text not found!", bodyText.contains(text));
+//        Assert(actualMovieTitle, movieName);
+
+//        Thread.sleep(1000);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 
-//		// Not needed but forcing the browser to wait for 4 seconds
-        Thread.sleep(4000);
+        //Check to see if the movie title matches with what the user searched for
+        //Assert.assertEquals(actualMovieTitle.replace("-", " ").toLowerCase(),movieName.replace("-", " ").toLowerCase());
+        Assert.assertEquals(handleMovieCase(actualMovieTitle),handleMovieCase(movieName));
 
-        // Check if title is Google
-        String actualTitle = driver.getTitle();
 
-//		System.out.println("Page title: " + actualTitle);
 
-//		Boolean result = actualTitle.equals("Google");
-//		System.out.println("Page title is Google ?  " + result);
 
-        if (actualTitle.contentEquals("Google")) {
-            System.out.println("Test Passed!");
-        } else {
-            System.out.println("Test Failed");
-        }
+//        driver.findElement(By.id("validationCustom02")).sendKeys("mminhaz@gmail.com");
+        
 
-        // close browser
-        driver.close();
+//        String name = "\"Minhaz\"";
+//        System.out.println(name);
 
+		Thread.sleep(3000);
+
+        //Closing browser
+        driver.quit(); // This closes the entire browser
+        // driver.close(); // This closes the active tab
+
+
+
+    }
+
+    private static String handleMovieCase(String str) {
+        String title = str.replace("-", " ").toLowerCase();
+
+        return title;
     }
 
 }
---
+++ src/main/java/temp.java
@@ -0,0 +1,45 @@
+public class temp {
+
+
+//        // Navigate to google
+//        driver.get("http://www.google.com"); // will navigate and wait till the entire page is loaded
+//        //driver.navigate().to("http://www.google.com"); // Take you to the page and not wait
+//
+//        //Find text box AND search for selenium
+////		driver.findElement(By.name("q")).sendKeys("Selenium");
+////		driver.findElement(By.name("q")).clear();
+////		driver.findElement(By.name("q")).sendKeys("Java");
+//
+//        //Optimized version
+//        WebElement search_box = driver.findElement(By.name("q"));
+//        search_box.sendKeys("Selenium");
+////		search_box.clear();
+////		search_box.sendKeys("Java");
+//
+//        //Find search button
+//        WebElement googleSearchButton = driver.findElement(By.xpath("//div[@class='tfB0Bf']//input[@name='btnK']"));
+//
+//        //Click search button - click()
+//        googleSearchButton.click();
+//
+//
+////		// Not needed but forcing the browser to wait for 4 seconds
+//        Thread.sleep(4000);
+//
+//        // Check if title is Google
+//        String actualTitle = driver.getTitle();
+//
+////		System.out.println("Page title: " + actualTitle);
+//
+////		Boolean result = actualTitle.equals("Google");
+////		System.out.println("Page title is Google ?  " + result);
+//
+//        if (actualTitle.contentEquals("Google")) {
+//            System.out.println("Test Passed!");
+//        } else {
+//            System.out.println("Test Failed");
+//        }
+//
+//        // close browser
+//        driver.close();
+}
